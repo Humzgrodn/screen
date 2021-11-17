4b4e23ed2e523f6f52cc09196f634706311cebf9
Zephyros256 <Robin.Kruit@hotmail.com>
2021-11-17 21:14:57 +0100

    XDG base directory instead of hard-coded

--- startscript.sh
+++ startscript.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-LOGFILE=/home/pi/screen-master/startstopscript.log
+LOGFILE=$HOME/screen-master/startstopscript.log
 RETAIN_NUM_LINES=20
 for ((n=0;n<5;n++))
 do
 mapfile -n 20 < $LOGFILE
-if ((${#MAPFILE[@]}>19)); then
+if ((${#MAPFILE[@]}>19)); then 
     sed -i '1d' $LOGFILE
 fi
 done
@@ -19,7 +19,7 @@ function log {
 log "Executing start screen command"
 sleep 2
 export DISPLAY=:0
-python3 /home/pi/screen-master/screenApp.py & 
+python3 $HOME/screen-master/screenApp.py & 
 log "start script done" 
 #the & is important for letting the startscript close.
 #the automatic shutdown problem at 15:05 has not yet been tested with this solution
